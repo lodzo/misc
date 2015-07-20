36721684ae67a407da3602079096b70516a88681
Herbert Burzlaff <HBurzlaff@synacor.com>
2015-07-20 16:11:48 -0400

    Fixing directory path in sed line

--- bash-it.sh
+++ bash-it.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh --none
-sed -i -e "/export BASH_IT_THEME/s/^.*$/export BASH_IT_THEME='doubletime_multiline'/" .bashrc
+sed -i -e "/export BASH_IT_THEME/s/^.*$/export BASH_IT_THEME='doubletime_multiline'/" ~/.bashrc
