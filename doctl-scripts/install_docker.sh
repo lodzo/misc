88b8c8ab1c0f76cc08543161d955b10d55f7c553
Herbert B <herbert.burzlaff@ryansg.com>
2018-10-02 09:47:48 -0400

    Fixing typo

--- doctl-scripts/install_docker.sh
+++ doctl-scripts/install_docker.sh
@@ -12,6 +12,6 @@ get_latest_release() {
     grep '"tag_name":' |                                            # Get tag line
     sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
 }
-DC_LATEST=$get_latest_release docker/compose)
+DC_LATEST=$(get_latest_release docker/compose)
 sudo curl -L "https://github.com/docker/compose/releases/download/$DC_LATEST/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
